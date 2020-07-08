d2ed286f4eac9ceb3e161b22c4b33eb7213d4758
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2020-07-08 01:23:24 -0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,24 @@
+export default {
+  mode: 'universal',
+  target: 'static',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: ['@nuxtjs/eslint-module'],
+  modules: ['nuxt-buefy', '@nuxtjs/pwa', '@nuxt/content'],
+  content: {},
+  build: {},
+}
